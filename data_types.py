b91a984934ad592864613889b3543bac17b59821
devendra-rgb <devendratumu@gmai..com>
2022-04-13 21:17:22 +0530

    added strings

---
+++ data_types.py
@@ -0,0 +1,25 @@
+#sentence 
+
+"""line 1
+line2  """
+
+a = -2
+
+a = 3
+
+b = 5.0
+
+d = None
+
+print(type(d))
+
+
+
+
+
+#print(type(c))
+
+
+
+#calling function 
+#function_name(arguments)
---
+++ strings_.py
@@ -0,0 +1,63 @@
+from re import A
+from tkinter import N
+
+
+name='Naveen'
+
+gender='Male'
+
+
+
+
+rank=1
+
+#new_rank=str(rank)
+
+#print(type(new_rank))
+
+#print(1)
+
+#
+# print(type(rank))
+ 
+print(name+str(rank)) #adding two different data types by converting one of them into string
+
+print(name+" is a "+gender)
+
+print("Happy Birthday "+ name)
+
+print(gender[0]) #indexing syntax
+
+print(gender[-3]) #reverse Indexing 
+
+# printing characters between limits
+
+print(name[1:]) #printing first indexing 
+
+print(name[:5]) #using the slicing 
+
+name="012345"
+print(name[::-1])
+
+age="       21    "
+
+print(age.strip()) # removing spaces both sides
+
+print(age.rstrip()) # removes commenting right side like that we can do lstrip
+
+quote= " it's me " 
+
+print(len(name)) # print the len 
+
+
+
+
+
+
+#N a v e e N
+#0 1 2 3 4 5
+
+
+
+
+
